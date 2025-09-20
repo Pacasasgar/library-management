dfef80dacf02be6bbd19789d2f567606ea8f9d97
Pau Casas Garcia <pacasasgar@gmail.com>
2025-09-20 02:05:44 +0200

    Implement green test

--- src/main/java/com/library/librarymanagement/RegisterMemberUseCase.java
+++ src/main/java/com/library/librarymanagement/RegisterMemberUseCase.java
@@ -1,7 +1,13 @@
 package com.library.librarymanagement;
 
+import java.util.UUID;
+
 public class RegisterMemberUseCase {
     public Member execute(String name, String email) {
-        return null;
+        Member member = new Member();
+        member.setMemberId(UUID.randomUUID().toString());
+        member.setName(name);
+        member.setEmail(email);
+        return member;
     }
 }
