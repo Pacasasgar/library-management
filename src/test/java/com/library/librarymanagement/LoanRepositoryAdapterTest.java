96faff94a1348ad76572ee057769ab1a4651f00b
Pau Casas Garcia <pacasasgar@gmail.com>
2025-10-01 19:40:41 +0200

    Fix a failing unitary test implementing loanMapper

--- src/test/java/com/library/librarymanagement/LoanRepositoryAdapterTest.java
+++ src/test/java/com/library/librarymanagement/LoanRepositoryAdapterTest.java
@@ -2,6 +2,7 @@ package com.library.librarymanagement;
 
 import com.library.librarymanagement.domain.Loan;
 import com.library.librarymanagement.infrastructure.persistence.*;
+import com.library.librarymanagement.infrastructure.persistence.mapper.LoanMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -22,23 +24,47 @@ public class LoanRepositoryAdapterTest {
     @Mock
     private LoanJpaRepository loanJpaRepository;
 
+    @Mock
+    private MemberJpaRepository memberJpaRepository;
+
+    @Mock
+    private BookJpaRepository bookJpaRepository;
+
+    @Mock
+    private LoanMapper loanMapper;
+
     @Test
     void findByBookId_returnsSuccessfully_whenBookIsFound() {
         String bookId = "book-123";
+        String memberId = "member-abc";
+        String loanId = "loan-xyz";
+
         BookEntity bookEntity = new BookEntity();
         bookEntity.setBookId(bookId);
 
         MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberId(memberId);
+
         LoanEntity fakeLoanEntity = new LoanEntity();
-        fakeLoanEntity.setLoanId("loan-abc");
+        fakeLoanEntity.setLoanId(loanId);
         fakeLoanEntity.setBook(bookEntity);
         fakeLoanEntity.setMember(memberEntity);
 
+        Loan expectedLoan = new Loan();
+        expectedLoan.setLoanId(loanId);
+        expectedLoan.setBookId(bookId);
+        expectedLoan.setMemberId(memberId);
+
         when(loanJpaRepository.findByBook_BookId(bookId)).thenReturn(Optional.of(fakeLoanEntity));
+        when(loanMapper.toDomain(fakeLoanEntity)).thenReturn(expectedLoan);
 
         Optional<Loan> resultOptional = loanRepositoryAdapter.findByBookId(bookId);
 
-        assertTrue(resultOptional.isPresent(), "Loan should not be empty.");
+        assertTrue(resultOptional.isPresent(), "The returned Optional should not be empty.");
 
+        Loan resultLoan = resultOptional.get();
+        assertEquals(expectedLoan.getLoanId(), resultLoan.getLoanId());
+        assertEquals(expectedLoan.getBookId(), resultLoan.getBookId());
+        assertEquals(expectedLoan.getMemberId(), resultLoan.getMemberId());
     }
 }
