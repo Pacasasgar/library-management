b1bd22e66058256dfd6a721305efcc62a885a91e
Pau Casas Garcia <pacasasgar@gmail.com>
2025-10-01 19:54:20 +0200

    Add field mappings to LoanMapper interface
    Mapper was not able to map correctly from Entity to Domain and we specified the correct field relation manually. It was detected by the loan integration tests whichs demonstrates how useful they are when you implement more changes to the original code.

--- src/main/java/com/library/librarymanagement/infrastructure/persistence/mapper/LoanMapper.java
+++ src/main/java/com/library/librarymanagement/infrastructure/persistence/mapper/LoanMapper.java
@@ -3,9 +3,13 @@ package com.library.librarymanagement.infrastructure.persistence.mapper;
 import com.library.librarymanagement.domain.Loan;
 import com.library.librarymanagement.infrastructure.persistence.LoanEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface LoanMapper {
     LoanEntity toEntity(Loan loan);
+
+    @Mapping(source = "member.memberId", target = "memberId")
+    @Mapping(source = "book.bookId", target = "bookId")
     Loan toDomain(LoanEntity entity);
 }
